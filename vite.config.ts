97e6a4bc942e3cdbd85438b6bb6fdfd16c332d85
bodemaxim <bodemaxim@gmail.com>
2025-07-03 20:27:14 +0300

    base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: '/managers-report',
+  base: '/product-card',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
